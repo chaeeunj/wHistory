83a98b68f23cb97fa4b831ec10a09c20cd5d2da6
chaeeunj <official2930@naver.com>
2022-08-30 04:08:56 +0900

    필요한 패키지 설치 후 아이콘 에러 발생

---
+++ whistory/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserLock } from '@fortawesome/free-regular-svg-icons';
+
+function Header() {
+  return (
+    <Wrapper>
+      <LockIcon icon={faUserLock} />
+      비밀 시청 기록
+    </Wrapper>
+  );
+}
+
+export default Header;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 10vh;
+  font-weight: bold;
+  text-align: left;
+  justify-content: left;
+  align-items: center;
+  display: flex;
+  background-color: #ffe9ae;
+  padding-left: 30px;
+`;
+
+const LockIcon = styled(FontAwesomeIcon)``;
